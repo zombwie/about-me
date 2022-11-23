86be863fc177b0532f99a9b7facaad159181a483
zombwie <oliver@onowingerei.com>
2022-11-23 09:12:23 +0100

    side counter med graph

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const { readFile } = require('fs')
 const { connect } = require('http2')
+const { connection } = require('mongoose')
 const app = express()
 const port = 25002
 var mysql = require('mysql')
 
 var conection = mysql.createConnection({
-    host: '65.108.15.66',
-    user: 'u90_2n3WDdYx1l',
-    password: 'z+R=D99h^ud!=Bzrov7d5e2F',
-    database: 's90_visitors'
+    host: '...',
+    user: '',
+    password: '',
+    database: ''
 })
 app.get('/', (req, res) => {
   readFile('public/index.html', (err, data) => {
@@ -23,20 +24,33 @@ app.get('/', (req, res) => {
   var sql = "UPDATE visits SET visitors = visitors + 1";
   conection.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Visitors updated");
   });
+  var sql = "INSERT INTO datovisits (dato, tall) VALUES ( NOW(), 'first')";
+  conection.query(sql, function (err, result) {
+    if (err) throw err;
+    });
 })
 
 app.get('/info', async function(req,res){
 conection.connect(function(err) {
   conection.query("SELECT * FROM visits", function (err, result, fields) {
-  console.log(result);
+  console.log("Site has been visited " + result[0].visitors + " times");
   res.status(200).json({info: result})
     if (err) throw err;
    });
 });  
 })
-
+app.get('/api', function(req, res){
+  console.log('tid1: ' + req.query.tid1)
+  console.log('tid2: ' + req.query.tid2)
+  var tid1 = req.query.tid1
+  var tid2 = req.query.tid2
+  conection.query("SELECT COUNT(*) AS TEST FROM datovisits WHERE dato BETWEEN '" + tid1 + "' AND '" + tid2 + "'", function (err, result, fields) {
+    console.log(result[0])
+    if (err) throw err;
+    res.send(result[0])
+  });
+});
 
 app.listen(port, () => {
   console.log(`About me app listening on port ${port}`)

f89928ed6d1b576b415d33f1077264857b0eb11b
zombwie <oliver@onowingerei.com>
2022-11-30 14:30:13 +0100

    fiksa små ting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Webhook, MessageBuilder } = require('discord-webhook-node');
 const hook = new Webhook("https://discord.com/api/webhooks/1021323340096471082/8q7T0KvytjoSM2IHYnSU8bVj1MxrjvRoAx_3klahN2h8uH4V1ctqZqPRhA8F76lhFBin");
 var mysql = require('mysql')
 
-var conection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: '65.108.15.66',
     user: 'u90_2n3WDdYx1l',
     password: 'z+R=D99h^ud!=Bzrov7d5e2F',
@@ -24,18 +24,18 @@ app.get('/', (req, res) => {
     res.end(data)
   })
   var sql = "UPDATE visits SET visitors = visitors + 1";
-  conection.query(sql, function (err, result) {
+  connection.query(sql, function (err, result) {
     if (err) throw err;
   });
   var sql = "INSERT INTO datovisits (dato, tall) VALUES ( NOW(), 'first')";
-  conection.query(sql, function (err, result) {
+  connection.query(sql, function (err, result) {
     if (err) throw err;
     });
 })
 
 app.get('/info', async function(req,res){
-conection.connect(function(err) {
-  conection.query("SELECT * FROM visits", function (err, result, fields) {
+connection.connect(function(err) {
+  connection.query("SELECT * FROM visits", function (err, result, fields) {
   console.log("Site has been visited " + result[0].visitors + " times");
   res.status(200).json({info: result})
     if (err) throw err;
@@ -47,7 +47,7 @@ app.get('/api', function(req, res){
   console.log('tid2: ' + req.query.tid2)
   var tid1 = req.query.tid1
   var tid2 = req.query.tid2
-  conection.query("SELECT COUNT(*) AS TEST FROM datovisits WHERE dato BETWEEN '" + tid1 + "' AND '" + tid2 + "'", function (err, result, fields) {
+  connection.query("SELECT COUNT(*) AS TEST FROM datovisits WHERE dato BETWEEN '" + tid1 + "' AND '" + tid2 + "'", function (err, result, fields) {
     console.log(result[0])
     if (err) throw err;
     res.send(result[0])
@@ -56,22 +56,22 @@ app.get('/api', function(req, res){
 
 app.get('/formapi', function(req, res){
   console.log('email: ' + req.query.email)
-  console.log('fedback: ' + req.query.fedback)
+  console.log('fedback: ' + req.query.feedback)
   var email = req.query.email
-  var fedback = req.query.fedback
+  var feedback = req.query.feedback
 
   const embed = new MessageBuilder()
   .setTitle('Nettside Fedback')
   .setAuthor( email, 'https://cdn.discordapp.com/embed/avatars/0.png', 'http://65.108.15.66:25002/')
   .setURL('http://65.108.15.66:25002/')
-  .addField('Inhold:', fedback, true)
+  .addField('Inhold:', feedback, true)
   .setColor('#00b0f4')
   .setThumbnail('https://cdn.discordapp.com/attachments/1021323293732651009/1045293651816894504/4kantLogo.png')
   .setTimestamp();
   
   hook.send(embed);
 
-  conection.query("INSERT INTO feedback (dato, email, feedback) VALUES ( NOW(), '" + email + "', '" + fedback + "')", function (err, result, fields) {
+  connection.query("INSERT INTO feedback (dato, email, feedback) VALUES ( NOW(), '" + email + "', '" + feedback + "')", function (err, result, fields) {
     if (err) throw err;
     res.sendStatus(200)
   });
